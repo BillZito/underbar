47585b82cab3bffef3c2842e8fa12e34a674c836
Bill Zito <bill.r.zito@gmail.com>
2016-08-20 11:22:40 -0700

    each completed

--- src/underbar.js
+++ src/underbar.js
@@ -49,6 +49,18 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    //if array, use for i less than collection's length to run func on each
+    if (Array.isArray(collection)){
+      for (var i = 0; i < collection.length; i++){
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      //else, it's an object, so use for key in object to run func on each val
+      for (var key in collection){
+        iterator(collection[key], key, collection);
+      }
+    }
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
